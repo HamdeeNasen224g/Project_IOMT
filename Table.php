18349dca1d18fa0c20e76fac2f66d2e842aba5f0
HamdeeNasen224g <hamdee.na@mail.wu.ac.th>
2021-08-24 20:05:35 +0700

    Update Table.php

--- Table.php
+++ Table.php
@@ -1,7 +1,4 @@
 
-<?php include 'db_con.php'; ?>
-<?php include 'head.html'; ?>
-<?php include 'header.html'; ?>
 
 <div class="container">
 
@@ -19,7 +16,7 @@
     </thead>
     <tbody>
     <?php 
-    $sql = "SELECT * FROM disease";
+    $sql = "select * from patient p join patient_mass join data d join blood ";
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
@@ -27,34 +24,14 @@
       while($row = $result->fetch_assoc()) {
         ?>
             <tr>
-                <td><?php echo $row['diseaseID']?> </td>
-                <td><a href="view.php" target="_blank" id="<?php echo $row['diseaseID'];?>"><?php echo $row['name']?> </a></td>
-                <td><?php echo $row['icd10']?></td>
-
+                <td><?php echo $row['hid']?> </td>
                 <?php  ?>
-                <td>
                 
-                  <?php $tdid = $row['TDID'];
-            $sql1 = "SELECT * FROM typedisease WHERE TDID LIKE $tdid";
-            $result1 = $conn->query($sql1);
-           // var_dump($sql);
-                       if ($result1->num_rows > 0) {
-            // output data of each row
-                while($row1 = $result1->fetch_assoc()) {
-                  
-                    echo "<br>".$row1["name"]."<br>";
-                }
-            }else {
-              echo "0 results";
-            }
-        ?>
-                
-              </td>
                 <td>
-                <button type="button" class="btn btn-primary btn-edit" id="<?php echo $row['diseaseID'];?>"> 
+                <button type="button" class="btn btn-primary btn-edit" id="<?php echo $row['hid'];?>"> 
                 EDIT</ion-icon>
                </button>
-                <button type="button" class="btn btn-danger btn-del" id="<?php echo $row['diseaseID'];?>" >DEL</button>
+                <button type="button" class="btn btn-danger btn-del" id="<?php echo $row['hid'];?>" >DEL</button>
                 </td>
             </tr>
         <?php
@@ -90,10 +67,3 @@
     $('#example').DataTable();
 } );
     </script>
-
-<?php include 'footer.html'; ?>
-</body>
-</html>
- <?php 
-$conn->close();
-?>
