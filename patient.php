b67e0b78fd35acfcc5ef2933b8686502a9723362
Mtantana <mantana.la@mail.wu.ac.th>
2021-08-31 03:19:00 +0700

    Update patient.php

--- patient.php
+++ patient.php
@@ -222,14 +222,6 @@
                       </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-8">
-                      <div class="form-group">
-                        <label>About Me</label>
-                        <textarea rows="4" cols="80" class="form-control" placeholder="Here can be your description" value="Mike">Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</textarea>
-                      </div>
-                    </div>
-                  </div>
                 </form>
               </div>
               <div class="card-footer">
