25cf18ca14285f951121f630a1c257c2d41913f7
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-09-01 05:38:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -189,8 +189,7 @@
               <div class="card-header">
                 <div class="col 6"></div>
                 <h5 class="card-category">Total BPM</h5>
-                <h3 class="card-title"><i class="tim-icons icon-heart-2 text-success"></i> BPM </h3>
-                <h1  class="card-title"> <span id ="bpm">/</span></h1>
+                <h3 class="card-title"><i class="tim-icons icon-heart-2 text-success"></i> BPM <span id ="bpm">/</span></h3>
               </div>
               
               <div class="card-body">
