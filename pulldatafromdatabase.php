41d56c77d3f804025823ed48f0245ae0b7045603
HamdeeNasen224g <hamdee.na@mail.wu.ac.th>
2021-09-01 04:48:01 +0700

    Update pulldatafromdatabase.php

--- pulldatafromdatabase.php
+++ pulldatafromdatabase.php
@@ -1,8 +1,7 @@
 <?php
     $bloodList = array();
          $sql = "SELECT * from patient p join patient_mass pm on p.hid = pm.hid join blood b on b.blood_id = p.blood_id";
-         $resultM = $conn->query($sql);
-      
+         $resultM = $conn->query($sql);    
          if ($resultM->num_rows > 0) {
            while($row = $resultM->fetch_assoc()) {
                 $blood_id = $row['blood_id'];
