8909c19e6dacda6cdad77f29dd00c9f88437dab4
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-31 20:13:28 +0700

    Create db_con.php

---
+++ db_con.php
@@ -0,0 +1,13 @@
+<?php 
+$servername = "projectiot.ck1dneket7id.ap-southeast-1.rds.amazonaws.com";
+$username = "dee";
+$password = "62112073";
+$dbname = "IoMT";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+?>
