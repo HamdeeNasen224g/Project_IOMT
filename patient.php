6a2e599d912582b32039ca8550aaa3a07513b4a3
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-31 22:38:15 +0700

    Update patient.php

--- patient.php
+++ patient.php
@@ -151,17 +151,6 @@
         </div>
       </div>
       <!-- End Navbar -->
-      <?php 
-         $bloodList = array();
-         $sql = "select * from patient p join patient_mass pm on p.hid = pm.hid join blood b on b.blood_id = p.blood_id";
-         $resultM = $conn->query($sql);
-         
-         if ($resultM->num_rows > 0) {
-           while($row = $resultM->fetch_assoc()) {
-             $bloodList[$row["blood_id"]] = $row["blood_name"]; 
-           }
-         }
-         ?>
       <!-- End Database -->
       <div class="content">
         <div class="row">
