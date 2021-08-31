6a501953d3198636d9dd3762d5846a29b4295bec
HamdeeNasen224g <hamdee.na@mail.wu.ac.th>
2021-08-31 22:45:13 +0700

    Update patient.php

--- patient.php
+++ patient.php
@@ -151,17 +151,11 @@
         </div>
       </div>
       <!-- End Navbar -->
+
+ <?php include 'pulldatafromdatabase.php'; ?>
 <<<<<<< HEAD
-      <?php 
-         $bloodList = array();
-         $sql = "SELECT * from patient p join patient_mass pm on p.hid = pm.hid join blood b on b.blood_id = p.blood_id";
-         $resultM = $conn->query($sql);
-         
-         if ($resultM->num_rows > 0) {
-           while($row = $resultM->fetch_assoc()) {
-             $bloodList[$row["blood_id"]] = $row["blood_name"]; 
-           }
-         }
+      <?php  
+        
          ?>
 =======
 >>>>>>> 6a2e599d912582b32039ca8550aaa3a07513b4a3
