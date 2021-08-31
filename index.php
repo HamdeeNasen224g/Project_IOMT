9d62619d2f5af4f3957bd3453c57153bc86e69df
Mtantana <mantana.la@mail.wu.ac.th>
2021-08-31 22:55:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -195,10 +195,11 @@
               <div class="card-header">
                 <!-- <h5 class="card-category"></h5>-->
                 <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary"></i> BPM </h3>
+                <iframe width="380" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1483314/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
               </div>
               <div class="card-body">
                
-                  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1483314/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+                  
                 
               </div>
             </div>
