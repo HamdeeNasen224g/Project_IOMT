9b6c184cc0600fe2cbcb7a9a0122a1445b8911d1
HamdeeNasen224g <hamdee.na@mail.wu.ac.th>
2021-09-01 05:47:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 =========================================================
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
+<?php include 'db_con.php'; ?>
+<?php include 'pulldatafromdatabase.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
