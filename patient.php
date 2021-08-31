c021a95b3b55f38c1226f6a84f7306d4c8e17f1a
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-31 20:56:47 +0700

    Update patient.php

--- patient.php
+++ patient.php
@@ -164,7 +164,7 @@
                     <div class="col-md-3 pr-md-1">
                       <div class="form-group">
                         <label>Hospital Number</label>
-                        <input type="text" class="form-control" disabled="" placeholder="Hospital Number" value="<?php echo $row['hid']?>" >
+                        <input type="text" class="form-control" disabled="" placeholder="<?php echo $row['hid']?>"  >
                       </div>
                     </div>
                     <div class="col-md-4 px-md-1">
