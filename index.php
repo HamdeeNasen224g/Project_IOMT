ab14d7b7744d9399ca0c08728e86ae6ebbb23c70
HamdeeNasen224g <hamdee.na@mail.wu.ac.th>
2021-09-01 20:19:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -211,7 +211,32 @@
               </div>
               <div class="card-body">
                 <div class="chart-area">
-                  <canvas id="myChart"></canvas>
+                <table class="table">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
                 </div>
               </div>
             </div>
