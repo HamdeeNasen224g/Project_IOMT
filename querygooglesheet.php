c37bbc82d5b8b3a56fbce5f921c423c9f5816e71
HamdeeNasen224g <hamdee.na@mail.wu.ac.th>
2021-09-01 16:23:35 +0700

    Update querygooglesheet.php

--- querygooglesheet.php
+++ querygooglesheet.php
@@ -4,3 +4,12 @@ $time = $_POST["ID"];
     echo $time;
   console.log($time);
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <base target="_top">
+  </head>
+  <body>
+    <?php  echo $time; ?>
+  </body>
+</html>
